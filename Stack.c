05e0fe7706f061a4d21e54f97acedcd76b8d4b29
zkfbdkf <zkfbdkf@gmail.com>
2017-01-21 21:39:16 +0900

    Stack.c
    make stack in C

---
+++ Stack.c
@@ -0,0 +1,43 @@
+#pragma warning(disable: 4996)
+#include <stdio.h>
+#define PUSH 1
+#define DELETE 2
+#define SHOW 3
+#define QUIT 4
+
+int get_a_cmd()
+{
+	int n;
+	return n;
+}
+
+int push(int *stack)
+{
+}
+
+
+void pop(int *stack)
+{
+	printf("The Stack is empty, no more delete.\n");
+}
+
+void show(int *stack)
+{
+	else
+	{
+	}
+}
+
+{
+	int cmd;
+	int stack[5];
+	do
+	{
+			break;
+			break;
+			break;
+			break;
+			break;
+		}
+
+}
